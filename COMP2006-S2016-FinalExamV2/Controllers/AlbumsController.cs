b0097c4894c6f0397d8a46b3cfc647f8430586d2
RJame <RJame@DESKTOP-I0L87UH>
2016-08-18 17:33:01 -0400

    Added the controllers and comments, added navbar links

---
+++ COMP2006-S2016-FinalExamV2/Controllers/AlbumsController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using COMP2006_S2016_FinalExamV2.Models;
+/*      Patrick Ross
+        200307049
+        August 18 2016
+        created page
+        This albums controller*/
+namespace COMP2006_S2016_FinalExamV2.Controllers
+{
+    public class AlbumsController : Controller
+    {
+        private MVCMusicStoreContext db = new MVCMusicStoreContext();
+
+        // GET: Albums
+        public ActionResult Index()
+        {
+            var albums = db.Albums.Include(a => a.Artist).Include(a => a.Genre);
+            return View(albums.ToList());
+        }
+
+        // GET: Albums/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Album album = db.Albums.Find(id);
+            if (album == null)
+            {
+                return HttpNotFound();
+            }
+            return View(album);
+        }
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ COMP2006-S2016-FinalExamV2/Controllers/ArtistsController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using COMP2006_S2016_FinalExamV2.Models;
+/*      Patrick Ross
+        200307049
+        August 18 2016
+        created page
+        This artists controller*/
+namespace COMP2006_S2016_FinalExamV2.Controllers
+{
+    public class ArtistsController : Controller
+    {
+        private MVCMusicStoreContext db = new MVCMusicStoreContext();
+
+        // GET: Artists
+        public ActionResult Index()
+        {
+            return View(db.Artists.ToList());
+        }
+
+        // GET: Artists/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Artist artist = db.Artists.Find(id);
+            if (artist == null)
+            {
+                return HttpNotFound();
+            }
+            return View(artist);
+        }
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
